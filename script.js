cdc1ccc710a1a8b3965700b2f282baaeee4c4b2c
faebsche <fabian.patterson@rwth-aachen.de>
2022-12-08 10:25:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var map = L.map('map', {
 	layers: [
-		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			'attribution': 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-		})
+		= L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'jpg'
+});
 	],
 	center: [51.505, -0.09],
 	zoom: 12
 });
+var marker = L.marker([51.5, -0.09]).addTo(map);
