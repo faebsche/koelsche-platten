e03e36fded0d31f66c425ad1b2bb8fd16eb7e014
faebsche <fabian.patterson@rwth-aachen.de>
2022-12-08 10:28:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var map = L.map('map', {
-	layers: [
-		= L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	layers: [L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 1,
