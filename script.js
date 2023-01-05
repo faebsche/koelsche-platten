19e0c868306d0c7317eac9c208dbc42accf353b8
faebsche <fabian.patterson@rwth-aachen.de>
2023-01-05 10:25:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var map = L.map('map', {
 	layers: [L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			'attribution': 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'}),
+			'attribution': 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'})],
 	center: [50.922118, 6.947867],
-	zoom: 12]
+	zoom: 12
 });
 var marker = L.marker([50.922118, 6.947867]).addTo(map);
 
